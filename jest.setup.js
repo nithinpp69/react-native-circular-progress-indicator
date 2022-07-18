395bc9284c6978331bdffc22bd39b4fd12d539ad
nithinpp69 <nithinpp69@gmail.com>
2022-07-18 19:49:24 +0530

    fix: add defineAnimation to test mocks

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,12 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable no-undef */
-jest.mock('react-native-reanimated', () =>
-  jest.requireActual('./node_modules/react-native-reanimated/mock')
-);
+jest.mock('react-native-reanimated', () => {
+  // eslint-disable-next-line max-len
+  const mocks = jest.requireActual(
+    './node_modules/react-native-reanimated/mock'
+  );
+  return {
+    ...mocks,
+    defineAnimation: () => {},
+  };
+});

922516d46b9df53e6066f5d36120ea8154200ca4
The One <56931230+ichanner@users.noreply.github.com>
2022-10-21 16:35:32 -0400

    fix

--- src/hooks/useAnimatedValue.ts
+++ src/hooks/useAnimatedValue.ts
@@ -97,7 +97,7 @@ export default function useAnimatedValue({
 
   const reAnimate = () => {
     resetAnimatedValue();
-    animateValue();
+    if(startPaused) animateValue();
   };
 
   const sortedStrokeColors = useMemo(() => {
